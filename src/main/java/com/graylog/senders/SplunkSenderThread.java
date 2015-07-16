00f90f7a5044f2c4a6e37bd882627c9d0f641e63
Lennart Koopmann <lennart@graylog.com>
2015-07-15 21:47:29 -0500

    make tcp connection handling and message buffering much more resilient

---
+++ src/main/java/com/graylog/senders/SplunkSenderThread.java
@@ -0,0 +1,108 @@
+/**
+ * This file is part of Graylog.
+ *
+ * Graylog is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Graylog is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Graylog.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package com.graylog.senders;
+
+import io.netty.channel.Channel;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicBoolean;
+import java.util.concurrent.locks.Condition;
+import java.util.concurrent.locks.ReentrantLock;
+
+public class SplunkSenderThread {
+
+    private static final Logger LOG = LoggerFactory.getLogger(SplunkSenderThread.class);
+    private final ReentrantLock lock;
+    private final Condition connectedCond;
+    private final AtomicBoolean keepRunning = new AtomicBoolean(true);
+    private final Thread senderThread;
+    private Channel channel;
+
+    public SplunkSenderThread(final BlockingQueue<String> queue) {
+        this.lock = new ReentrantLock();
+        this.connectedCond = lock.newCondition();
+
+        this.senderThread = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                String message = null;
+
+                while (keepRunning.get()) {
+                    // wait until we are connected to the Splunk server before polling log events from the queue
+                    lock.lock();
+                    try {
+                        while (channel == null || !channel.isActive()) {
+                            try {
+                                connectedCond.await();
+                            } catch (InterruptedException e) {
+                                if (!keepRunning.get()) {
+                                    // bail out if we are awoken because the application is stopping
+                                    break;
+                                }
+                            }
+                        }
+                        // we are connected, let's start sending logs
+                        try {
+                            // if we have a lingering event already, try to send that instead of polling a new one.
+                            if (message == null) {
+                                message = queue.poll(100, TimeUnit.MILLISECONDS);
+                            }
+                            // if we are still connected, convert LoggingEvent to Splunk and send it
+                            // but if we aren't connected anymore, we'll have already pulled an event from the queue,
+                            // which we keep hanging around in this thread and in the next loop iteration will block until we are connected again.
+                            if (message != null && channel != null && channel.isActive()) {
+                                // Write the Splunk message to the pipeline. The protocol specific channel handler
+                                // will take care of encoding.
+                                channel.writeAndFlush(message);
+                                message = null;
+                            }
+                        } catch (InterruptedException e) {
+                            // ignore, when stopping keepRunning will be set to false outside
+                        }
+                    } finally {
+                        lock.unlock();
+                    }
+                }
+
+                LOG.debug("SplunkSenderThread exiting!");
+            }
+        });
+
+        this.senderThread.setName("SplunkSenderThread-" + senderThread.getId());
+    }
+
+    public void start(Channel channel) {
+        lock.lock();
+        try {
+            this.channel = channel;
+            this.connectedCond.signalAll();
+        } finally {
+            lock.unlock();
+        }
+        senderThread.start();
+    }
+
+    public void stop() {
+        keepRunning.set(false);
+        senderThread.interrupt();
+    }
+
+}
--- src/main/java/com/graylog/senders/TCPSender.java
+++ src/main/java/com/graylog/senders/TCPSender.java
@@ -16,8 +16,8 @@
  */
 package com.graylog.senders;
 
-import com.google.common.base.Charsets;
 import io.netty.bootstrap.Bootstrap;
+import io.netty.buffer.ByteBuf;
 import io.netty.channel.*;
 import io.netty.channel.nio.NioEventLoopGroup;
 import io.netty.channel.socket.SocketChannel;
@@ -27,7 +27,11 @@ import org.graylog2.plugin.Message;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.net.InetSocketAddress;
 import java.util.Map;
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.LinkedBlockingQueue;
+import java.util.concurrent.TimeUnit;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -40,77 +44,102 @@ public class TCPSender implements Sender {
     private final String hostname;
     private final int port;
 
-    private Channel channel = null;
-    private EventLoopGroup group;
+    boolean initialized = false;
 
-    private Bootstrap bootstrap;
+    protected final BlockingQueue<String> queue;
+
+    private final EventLoopGroup workerGroup = new NioEventLoopGroup();
 
     public TCPSender(String hostname, int port) {
         this.hostname = hostname;
         this.port = port;
+
+        /*
+          * This internal queue shields us from causing OutputBufferProcessor
+          * timeouts for a short time without risking memory overload or
+          * loosing messages in case of temporary connection problems.
+          *
+          * TODO: Make configurable.
+          */
+        this.queue = new LinkedBlockingQueue<>(512);
+    }
+
+    protected void createBootstrap(final EventLoopGroup workerGroup) {
+        final Bootstrap bootstrap = new Bootstrap();
+        final SplunkSenderThread senderThread = new SplunkSenderThread(queue);
+
+        bootstrap.group(workerGroup)
+                .channel(NioSocketChannel.class)
+                .option(ChannelOption.CONNECT_TIMEOUT_MILLIS, 5000)
+                .remoteAddress(new InetSocketAddress(hostname, port))
+                .handler(new ChannelInitializer<SocketChannel>() {
+                    @Override
+                    protected void initChannel(SocketChannel ch) throws Exception {
+                        ch.pipeline().addLast(new StringEncoder());
+
+                        ch.pipeline().addLast(new SimpleChannelInboundHandler<ByteBuf>() {
+                            @Override
+                            protected void channelRead0(ChannelHandlerContext ctx, ByteBuf msg) throws Exception {
+                              // we only send data, never read on the socket
+                            }
+
+                            @Override
+                            public void channelActive(ChannelHandlerContext ctx) throws Exception {
+                                senderThread.start(ctx.channel());
+                            }
+
+                            @Override
+                            public void channelInactive(ChannelHandlerContext ctx) throws Exception {
+                                LOG.info("Channel disconnected.");
+                                senderThread.stop();
+                                scheduleReconnect(ctx.channel().eventLoop());
+                            }
+
+                            @Override
+                            public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) throws Exception {
+                                LOG.error("Exception caught", cause);
+                            }
+                        });
+                    }
+                });
+
+        bootstrap.connect().addListener(new ChannelFutureListener() {
+            @Override
+            public void operationComplete(ChannelFuture future) throws Exception {
+                if (future.isSuccess()) {
+                    LOG.info("Connected.");
+                } else {
+                    LOG.error("Connection failed: {}", future.cause().getMessage());
+                    scheduleReconnect(future.channel().eventLoop());
+                }
+            }
+        });
+    }
+
+    protected void scheduleReconnect(final EventLoopGroup workerGroup) {
+        workerGroup.schedule(new Runnable() {
+            @Override
+            public void run() {
+                LOG.info("Starting reconnect!");
+                createBootstrap(workerGroup);
+            }
+        }, 1000, TimeUnit.MILLISECONDS);
     }
 
     @Override
     public void initialize() {
-        connect();
-    }
+        createBootstrap(workerGroup);
 
-    private void connect() {
-        try {
-            LOG.info("Connecting to Splunk.");
-            group = new NioEventLoopGroup();
-            bootstrap = new Bootstrap();
-            bootstrap.group(group).channel(NioSocketChannel.class);
-
-            bootstrap.option(ChannelOption.SO_KEEPALIVE, true);
-            bootstrap.handler(new ChannelInitializer<SocketChannel>() {
-                @Override
-                public void initChannel(SocketChannel ch) throws Exception {
-                    ch.pipeline().addLast(new StringEncoder(Charsets.UTF_8));
-                }
-            });
-
-            channel = bootstrap.connect(hostname, port).sync().channel();
-        } catch (Exception e) {
-            LOG.error("Error when trying to connect to Splunk.", e);
-            disconnect();
-        }
-    }
-
-    private void disconnect() {
-        try {
-            if (channel != null) {
-                channel.close();
-                channel.deregister();
-            }
-        } catch(Exception e) {
-            LOG.warn("Could not close channel. Attempting graceful shutdown of eventloop group next.");
-        } finally {
-            if (group != null) {
-                group.shutdownGracefully();
-                LOG.debug("Eventloop group shutdown.");
-            }
-        }
+        initialized = true;
     }
 
     @Override
     public void stop() {
-        disconnect();
+        workerGroup.shutdownGracefully();
     }
 
     @Override
     public void send(Message message) {
-        if(!isConnected()) {
-            LOG.info("Channel not open. Reconnecting.");
-            disconnect();
-            connect();
-
-            if (!isInitialized()) {
-                LOG.warn("Channel still not open. Rejecting this message.");
-                return;
-            }
-        }
-
         StringBuilder splunkMessage = new StringBuilder();
         splunkMessage.append(message.getTimestamp().toString("yyyy/MM/dd-HH:mm:ss.SSS"))
                 .append(" ")
@@ -125,18 +154,20 @@ public class TCPSender implements Sender {
             splunkMessage.append(" ").append(field.getKey()).append("=").append(escape(field.getValue()));
         }
 
-        if (isInitialized()) {
-            channel.writeAndFlush(splunkMessage.append("\r\n").toString());
+        splunkMessage.append("\r\n");
+
+        LOG.debug("Sending message: {}", splunkMessage);
+
+        try {
+            queue.put(splunkMessage.toString());
+        } catch (InterruptedException e) {
+            LOG.warn("Interrupted. Message was most probably lost.");
         }
     }
 
     @Override
     public boolean isInitialized() {
-        return channel != null && channel.isActive();
-    }
-
-    private boolean isConnected() {
-        return isInitialized();
+        return initialized;
     }
 
     private Object noNewLines(Object value) {
@@ -160,7 +191,7 @@ public class TCPSender implements Sender {
             Matcher matcher = WHITESPACE_PATTERN.matcher((String) value);
             if (matcher.find()) {
                 StringBuilder sb = new StringBuilder()
-                    .append("\"").append(value).append("\"");
+                        .append("\"").append(value).append("\"");
                 value = sb.toString();
             }
         }
